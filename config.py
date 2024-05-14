5273d13ea81d27d1a42bd0ad5636086fd06c353f
Lord <110597375+kd2929@users.noreply.github.com>
2024-05-14 09:27:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 29707621
-api_hash = "a1f31eeb31a0b9e36f551da6b8b85752"
-bot_token = "6634971116:AAGhU6IQBREjXdliqJr9CGh1ZCLpoYbGP5o"
-auth_users = "6707073614, 1406300051, 6801378994, 870101201"
+api_id = 24250238
+api_hash = "cb3f118ce5553dc140127647edcf3720"
+bot_token = "5827671272:AAEdqmWvjRtp8_Ry3gp6SOo2jcgCnPW8Ljw"
+auth_users = "6175650047"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6707073614"
+osowner_users = "6175650047"
 owner_users = [int(num) for num in osowner_users.split(",")]
